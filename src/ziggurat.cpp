eb48fa39d4b061ef437a219253c6e0ec0391bb88
Dirk Eddelbuettel <edd@debian.org>
2025-01-05 16:32:32 -0600

    Remove (lightweight) use of tidyCpp, now zero-depends
    Also remove two dot-files not needed in example package

--- src/ziggurat.cpp
+++ src/ziggurat.cpp
@@ -1,10 +1,8 @@
 
 #include <zigg>
-#include <tidy/globals.h>       // instead of header tidyCpp itself we include a subset directly
-#include <tidy/defines.h>
-#include <tidy/internals.h>
-#include <tidy/numvec.h>
-#include <tidy/protect.h>
+
+#include <R.h>
+#include <Rinternals.h>         					// for SEXP
 
 typedef SEXP sexp;
 
@@ -13,34 +11,40 @@ static zigg::Ziggurat ziggurat; 					// static instance ensures initialization
 extern "C" {
 
 sexp zrnorm(sexp ns) {
-    int n = R::asInteger(ns);
-    R::Protect v = R::allocVectorNumeric(n);
-    double* p = R::numericPointer(v);
-    for (int i = 0; i < n; i++)
+    int n = Rf_asInteger(ns);
+    SEXP v = PROTECT(Rf_allocVector(REALSXP, n));
+    double* p = REAL(v);
+    for (int i = 0; i < n; i++) {
         p[i] = ziggurat.rnorm();
+    }
+    UNPROTECT(1);
     return v;
 }
 
 sexp zrexp(sexp ns) {
-    int n = R::asInteger(ns);
-    R::Protect v = R::allocVectorNumeric(n);
-    double* p = R::numericPointer(v);
-    for (int i = 0; i < n; i++)
+    int n = Rf_asInteger(ns);
+    SEXP v = PROTECT(Rf_allocVector(REALSXP, n));
+    double* p = REAL(v);
+    for (int i = 0; i < n; i++) {
         p[i] = ziggurat.rexp();
+    }
+    UNPROTECT(1);
     return v;
 }
 
 sexp zrunif(sexp ns) {
-    int n = R::asInteger(ns);
-    R::Protect v = R::allocVectorNumeric(n);
-    double* p = R::numericPointer(v);
-    for (int i = 0; i < n; i++)
+    int n = Rf_asInteger(ns);
+    SEXP v = PROTECT(Rf_allocVector(REALSXP, n));
+    double* p = REAL(v);
+    for (int i = 0; i < n; i++) {
         p[i] = ziggurat.runi();
+    }
+    UNPROTECT(1);
     return v;
 }
 
 sexp zsetseed(sexp ss) {
-    double sd = R::asNumeric(ss);
+    double sd = Rf_asReal(ss);
     uint32_t su = static_cast<uint32_t>(sd);
     ziggurat.setSeed(su);
     return R_NilValue;
