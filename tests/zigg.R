d775a35e5a841e72a44dd8cf1fc8a4360bfde52c
Dirk Eddelbuettel <edd@debian.org>
2025-01-27 10:38:49 -0600

    Tighten formatting in test file and reference output

--- tests/zigg.R
+++ tests/zigg.R
@@ -1,17 +1,18 @@
 
 library(zigg)
+options(digits=6, scipen=99)
 
 N <- 1e5
 zsetseed(12345)
 
 rn <- zrnorm(N)
-summary(rn)
+round(summary(rn), digits=4)
 
 re <- zrexp(N)
-summary(re)
+round(summary(re), digits=4)
 
 ru <- zrunif(N)
-summary(ru)
+round(summary(ru), digits=4)
 
 zsetseed(12345)
 all.equal(rn, zrnorm(N))
